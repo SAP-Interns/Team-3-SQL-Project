10d5c96433a18a560432155f687082ad55a17a99
MalMikullovci <602000923@genpact.com>
2026-04-01 14:24:11 +0200

    smol changes

--- Phase_3/KPIs/top_n_products.sql
+++ Phase_3/KPIs/top_n_products.sql
@@ -10,10 +10,10 @@ FROM (
             ORDER BY SUM(oli.TotalPrice) DESC
         ) AS rnk
     FROM OrderLineItem oli
-    JOIN SaleOrder so ON oli.SaleOrderID = so.ID
-    JOIN Customer c ON so.CustomerID = c.ID
-    JOIN Country ctry ON c.CountryID = ctry.ID
-    JOIN Product p ON oli.ProductID = p.ID
+    INNER JOIN SaleOrder so ON oli.SaleOrderID = so.ID
+    INNER JOIN Customer c ON so.CustomerID = c.ID
+    INNER JOIN Country ctry ON c.CountryID = ctry.ID
+    INNER JOIN Product p ON oli.ProductID = p.ID
     WHERE so.OrderDate BETWEEN '2025-01-01' AND '2025-12-31'
     GROUP BY ctry.Name, p.Name
 ) t
